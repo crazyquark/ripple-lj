33f5f7456dd0db3fa0c088918502e693cb0b770a
crazyquark <sam.bitcoin@gmail.com>
2016-02-08 13:04:42 +0200

    cleanup

--- src/main/java/com/ripple/client/RippleClient.java
+++
@@ -1,5 +0,0 @@
-package com.ripple.client;
-
-public class RippleClient {
-
-}
---
+++ src/main/java/com/ripple/client/rpc/RippleRpcClient.java
@@ -0,0 +1,11 @@
+package com.ripple.client.rpc;
+
+import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
+
+public class RippleRpcClient {
+	private JsonRpcHttpClient jsonRpcClient;
+	
+	public RippleRpcClient(String host, int port) {
+		
+	}
+}
