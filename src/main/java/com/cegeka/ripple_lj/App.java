2e1ca388940b95fda519d87d0bf6bfecf6b8db6e
crazyquark <sam.bitcoin@gmail.com>
2016-02-08 12:42:16 +0200

    Create Eclipse Maven project

---
+++ src/main/java/com/cegeka/ripple_lj/App.java
@@ -0,0 +1,13 @@
+package com.cegeka.ripple_lj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
